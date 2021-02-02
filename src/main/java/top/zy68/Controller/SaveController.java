58f525d75aaecbfaef87fdb3ff18fb51af7df567
MrGo123 <zymail68@foxmail.com>
2021-02-02 17:21:35 +0800

    build(frontend and backend separate):achieve the separation of of frontend and backend

--- src/main/java/top/zy68/Controller/SaveController.java
+++ src/main/java/top/zy68/Controller/SaveController.java
@@ -17,20 +17,10 @@ import javax.servlet.http.HttpServletRequest;
  * @Date2021/1/21 14:41
  * @Version 1.0
  **/
-@Controller
+@RestController
 public class SaveController {
     @Autowired
     private HandleDataService handleDataService;
-
-    /**
-     * home page controller
-     * Mapping to index.html
-     * @return String
-     */
-    @RequestMapping(value = "/")
-    public String indexCtrl() {
-        return "forward:index.html";
-    }
 
     /**
      * Handling the data from frontend by ajax
@@ -40,14 +30,13 @@ public class SaveController {
      * @return String
      */
     @RequestMapping(value = "save", method = RequestMethod.POST)
-    @ResponseBody
     public String getPasteData(@RequestParam(value = "pasteCode") String pasteCode,
                                @RequestParam(value = "saveTime") int saveTime,
                                HttpServletRequest request) {
         // 通过ipUtil拿到client的ip，并将此IP直接传给数据生成服务。
         String clientIp = IpUtil.getIpAddress(request);
-        return "done";
-        // return handleDataService.generateRecord(saveTime,pasteCode,clientIp);
+        // return "done";
+        return handleDataService.generateRecord(saveTime,pasteCode,clientIp);
     }
 
 
