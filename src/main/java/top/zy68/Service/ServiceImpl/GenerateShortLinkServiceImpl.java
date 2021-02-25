03c2ee7082973fe48f949ed8a4c47cc81db59a61
MrGo123 <zymail68@foxmail.com>
2021-02-25 11:12:37 +0800

    :recycle: Refactoring code.

--- src/main/java/top/zy68/Service/ServiceImpl/GenerateShortLinkServiceImpl.java
+++ src/main/java/top/zy68/Service/ServiceImpl/GenerateShortLinkServiceImpl.java
@@ -1,10 +1,15 @@
 package top.zy68.Service.ServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import top.zy68.Service.GenerateShortLinkService;
+import top.zy68.Utils.Base62EncodingUtil;
 
+import javax.annotation.Resource;
+import javax.xml.ws.spi.WebServiceFeatureAnnotation;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
@@ -15,66 +20,16 @@ import java.nio.charset.StandardCharsets;
  * @Date2021/1/25 11:39
  * @Version 1.0
  **/
-@Service
-@Component
+@Service("GenerateShortLinkService")
 public class GenerateShortLinkServiceImpl implements GenerateShortLinkService {
 
     @Override
     public String encryptByMd5DigestUtils(String clientIpTimestamp) {
-        String md5 = "";
-        md5 = DigestUtils.md5DigestAsHex(clientIpTimestamp.getBytes(StandardCharsets.UTF_8));
-        return md5;
+        return DigestUtils.md5DigestAsHex(clientIpTimestamp.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
     public String getShortLinkFromBase62Encoding(String resultOfMd5) {
-
-        return null;
+        return Base62EncodingUtil.getSevenCharacters(resultOfMd5);
     }
-    // public static StringBuffer encodeBase62(byte[] data) {
-    //     StringBuffer sb = new StringBuffer(data.length * 2);
-    //     int pos = 0, val = 0;
-    //     for (int i = 0; i < data.length; i++) {
-    //         val = (val << 8) | (data[i] & 0xFF);
-    //         pos += 8;
-    //         while (pos > 5) {
-    //             char c = encodes[val >> (pos -= 6)];
-    //             sb.append(
-    //                     /**/c == 'i' ? "ia" :
-    //                             /**/c == '+' ? "ib" :
-    //                             /**/c == '/' ? "ic" : c);
-    //             val &= ((1 << pos) - 1);
-    //         }
-    //     }
-    //     if (pos > 0) {
-    //         char c = encodes[val << (6 - pos)];
-    //         sb.append(
-    //                 /**/c == 'i' ? "ia" :
-    //                         /**/c == '+' ? "ib" :
-    //                         /**/c == '/' ? "ic" : c);
-    //     }
-    //     return sb;
-    // }
-    //
-    // public static byte[] decodeBase62(char[] data) {
-    //     ByteArrayOutputStream baos = new ByteArrayOutputStream(data.length);
-    //     int pos = 0, val = 0;
-    //     for (int i = 0; i < data.length; i++) {
-    //         char c = data[i];
-    //         if (c == 'i') {
-    //             c = data[++i];
-    //             c =
-    //                     /**/c == 'a' ? 'i' :
-    //                     /**/c == 'b' ? '+' :
-    //                     /**/c == 'c' ? '/' : data[--i];
-    //         }
-    //         val = (val << 6) | decodes[c];
-    //         pos += 6;
-    //         while (pos > 7) {
-    //             baos.write(val >> (pos -= 8));
-    //             val &= ((1 << pos) - 1);
-    //         }
-    //     }
-    //     return baos.toByteArray();
-    // }
 }
--- src/test/java/top/zy68/Service/ServiceImpl/generateShortLinkServiceImplTest.java
+++ src/test/java/top/zy68/Service/ServiceImpl/generateShortLinkServiceImplTest.java
@@ -1,22 +1,37 @@
 package top.zy68.Service.ServiceImpl;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.apache.ibatis.scripting.xmltags.ForEachSqlNode;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.annotation.ApplicationScope;
 import top.zy68.Service.GenerateShortLinkService;
+import top.zy68.Utils.Base62EncodingUtil;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
+@SpringBootTest
 class generateShortLinkServiceImplTest {
 
+    @Autowired
+    GenerateShortLinkServiceImpl generateShortLinkService;
 
-    GenerateShortLinkService generateShortLinkService = new GenerateShortLinkServiceImpl();
 
     @Test
     void generateByMD5DigestUtils() {
-        System.out.println(generateShortLinkService.encryptByMd5DigestUtils("Marydon"));
+        String hex128bits = generateShortLinkService.encryptByMd5DigestUtils("1921081261261612245885241");
+        System.out.println(hex128bits);
     }
 
     // @Test
     void getShortLinkFromBase62Encoding() {
+        // System.out.println(generateShortLinkService.getShortLinkFromBase62Encoding(
+        //         generateShortLinkService.encryptByMd5DigestUtils("Marydon")));
+
+        // System.out.println(Arrays.toString(Base62EncodingUtil.decodeBase62("hello")));
     }
 }
