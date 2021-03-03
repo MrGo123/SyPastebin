5072be322a03f9a398ece26e22d0bd3b009813ab
MrGo123 <zymail68@foxmail.com>
2021-03-04 01:26:35 +0800

    :white_check_mark: Adding tests.

---
+++ src/test/java/top/zy68/Dao/PasteMapperTest.java
@@ -0,0 +1,48 @@
+package top.zy68.Dao;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import top.zy68.Model.Paste;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PasteMapperTest {
+
+    @Autowired
+    PasteMapper pasteMapper;
+
+    private final static Logger log = LoggerFactory.getLogger(PasteMapperTest.class);
+    // @Test
+    void deleteByPrimaryKey() {
+    }
+
+    // @Test
+    void insert() {
+    }
+
+    // @Test
+    void insertSelective() {
+    }
+
+    @Test
+    void selectByPrimaryKey() {
+        Paste paste = pasteMapper.selectByPrimaryKey("UUUUUUU");
+        if(paste == null){
+            log.info("it is null");
+        }else{
+            log.info(paste.toString());
+        }
+
+    }
+
+    // @Test
+    void updateByPrimaryKeySelective() {
+    }
+
+    // @Test
+    void updateByPrimaryKey() {
+    }
+}
