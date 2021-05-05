933236bd00a5a96158c467cc57a46cdb642345e6
MrGo123 <zymail68@foxmail.com>
2021-05-05 15:34:04 +0800

    :sparkles: Refactor and add two method from HandleDataService.

--- src/main/java/top/zy68/Service/GenerateShortLinkService.java
+++ src/main/java/top/zy68/Service/GenerateShortLinkService.java
@@ -3,16 +3,30 @@ package top.zy68.Service;
 
 public interface GenerateShortLinkService {
     /**
-     * encoding the IP and timestamp by MD5
+     * encoding the IP and timestamp by MD5.
      * @param clientIpTimestamp
      * @return
      */
     String encryptByMd5DigestUtils(String clientIpTimestamp);
 
     /**
-     * generate the 7 characters by Base62 based on the result of MD5
+     * generate the 7 characters by Base62 based on the result of MD5.
      * @param resultOfMd5
      * @return
      */
     String getShortLinkFromBase62Encoding(String resultOfMd5);
+
+    /**
+     * generate shortLink by clientIp and timestamp.
+     * @param clientIp
+     * @return
+     */
+    String generateShortLink(String clientIp);
+
+    /**
+     * judge whether the shortLink is existed.
+     * @param shortLink
+     * @return
+     */
+    boolean shortLinkNotExist(String shortLink);
 }
--- src/main/java/top/zy68/Service/ServiceImpl/GenerateShortLinkServiceImpl.java
+++ src/main/java/top/zy68/Service/ServiceImpl/GenerateShortLinkServiceImpl.java
@@ -1,16 +1,12 @@
 package top.zy68.Service.ServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+import top.zy68.Dao.PasteMapper;
+import top.zy68.Model.Paste;
 import top.zy68.Service.GenerateShortLinkService;
 import top.zy68.Utils.Base62EncodingUtil;
-
-import javax.annotation.Resource;
-import javax.xml.ws.spi.WebServiceFeatureAnnotation;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -22,6 +18,8 @@ import java.nio.charset.StandardCharsets;
  **/
 @Service("GenerateShortLinkService")
 public class GenerateShortLinkServiceImpl implements GenerateShortLinkService {
+    @Autowired
+    PasteMapper pasteMapper;
 
     @Override
     public String encryptByMd5DigestUtils(String clientIpTimestamp) {
@@ -32,4 +30,45 @@ public class GenerateShortLinkServiceImpl implements GenerateShortLinkService {
     public String getShortLinkFromBase62Encoding(String resultOfMd5) {
         return Base62EncodingUtil.getSevenCharacters(resultOfMd5);
     }
+    /**
+     * 根据IP和时间戳生成短连接shortLink
+     * @param clientIp
+     * @return
+     */
+    @Override
+    public String generateShortLink(String clientIp){
+        String shortLink = "";
+
+        while (true) {
+            // 生成IP和时间戳。+7是为了保证shortLink不符合要求后重新得到的时间戳时间跨度加大，使得新ipTimestamp和之前的有较大差异。
+            String ipTimestamp = clientIp + System.currentTimeMillis() + 7;
+
+            // 使用md5和Base62生成一个shortLink
+            shortLink = getShortLinkFromBase62Encoding(encryptByMd5DigestUtils(ipTimestamp));
+
+            // 判断生成的shortLink是否够7个字符，不够则重新生成。正常不会发生，除非md5生成的散列码转换为十进制数过小。
+            if (shortLink.length() != 7) {
+                continue;
+            }
+
+            // 检查mysql是否已存在此shortLink
+            if (shortLinkNotExist(shortLink)) {
+                break;
+            }
+        }
+
+        return shortLink;
+    }
+
+    /**
+     * 判断shortLink是否已存在,false-存在；true-不存在
+     * @param shortLink
+     * @return
+     */
+    @Override
+    public boolean shortLinkNotExist(String shortLink){
+        Paste tempPaste = pasteMapper.selectByPrimaryKey(shortLink);
+        return tempPaste == null;
+    }
+
 }
